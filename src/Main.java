b5ab5057710aad5cf3294bf19a13e4615e2689cb
Illia Tkachov <i.o.tkachov@gmail.com>
2024-04-17 21:55:47 +0300

    add my name

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Illia, im Java Teacher.");
         System.out.println("List of my students:");
+
     }
 }
